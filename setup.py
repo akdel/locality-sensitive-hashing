57ff54049910c016a85247576cf190bbc6aab3eb
Akdel <mehmet_akdel@hotmail.com>
2018-10-22 22:30:34 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='Simple-LSH',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Mehmet Akdel',
+      author_email='akdel.mehmet@gmail.com',
+      url='',
+      packages=['LSH'],
+     )
