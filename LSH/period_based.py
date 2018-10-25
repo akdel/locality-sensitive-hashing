a739e03444af773e66e939d267a57de1a3554460
Akdel <mehmet_akdel@hotmail.com>
2018-10-25 11:01:24 +0200

    first commit

--- LSH/period_based.py
+++ LSH/period_based.py
@@ -3,14 +3,14 @@ from LSH.lsh import VecorsInLSH
 
 
 class Periods:
-    def __init__(self, list_of_coords, bin_list=(0, 0.001, 3, 6, 9, 12, 18, 25, 35), max_period_len=50):
+    def __init__(self, list_of_coords, bin_list=(0, 0.001, 3, 6, 9, 12, 18, 25, 35), max_period_len=25):
         self.bin_array = np.array(list(bin_list), dtype=np.float64)
         self.periods = multiple_coordinates_to_periods(list_of_coords, max_len=max_period_len)
         self.vectors, self.ranges = np.histogram(self.periods, bins=self.bin_array)
 
 
-def periods_to_hash_buckets(list_of_coords, number_of_buckets):
-    p = Periods(list_of_coords)
+def periods_to_hash_buckets(list_of_coords, number_of_buckets, max_period_len=100):
+    p = Periods(list_of_coords, max_period_len=max_period_len)
     periods = p.periods
     return VecorsInLSH(number_of_buckets, periods)
 
