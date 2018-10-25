9cc968903fe3639c20e19117dc4774c195412029
Akdel <mehmet_akdel@hotmail.com>
2018-10-25 11:30:31 +0200

    first commit

--- LSH/period_based.py
+++ LSH/period_based.py
@@ -13,7 +13,7 @@ class Periods:
 
 def periods_to_hash_buckets(list_of_coords, number_of_buckets, max_period_len=100):
     p = Periods(list_of_coords, max_period_len=max_period_len)
-    return VecorsInLSH(number_of_buckets, p.vectors)
+    return VecorsInLSH(number_of_buckets, list(p.vectors))
 
 
 def coordinates_to_periods(coordinates):
