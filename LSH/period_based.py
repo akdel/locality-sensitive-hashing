dae68657b74877e6bbb28c193801ff772194ad30
Akdel <mehmet_akdel@hotmail.com>
2018-10-25 10:59:34 +0200

    first commit

--- LSH/period_based.py
+++ LSH/period_based.py
@@ -1,12 +1,12 @@
-from LSH.numba_helpers import *
+from LSH.LSH.numba_helpers import *
 from LSH.lsh import VecorsInLSH
 
 
 class Periods:
-    def __init__(self, list_of_coords, bin_list=(0, 3, 6, 9, 12, 18, 25, 35), max_period_len=9):
+    def __init__(self, list_of_coords, bin_list=(0, 0.001, 3, 6, 9, 12, 18, 25, 35), max_period_len=50):
         self.bin_array = np.array(list(bin_list), dtype=np.float64)
         self.periods = multiple_coordinates_to_periods(list_of_coords, max_len=max_period_len)
-        pass
+        self.vectors, self.ranges = np.histogram(self.periods, bins=self.bin_array)
 
 
 def periods_to_hash_buckets(list_of_coords, number_of_buckets):
